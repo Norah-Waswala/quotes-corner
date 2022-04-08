e80713af0aaeeaecc9f211027f46ca7541d7dc2e
Norah.Waswala <norah.waswala@student.moringa.com>
2022-04-08 16:38:27 +0300

    feat:component quotes details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuotesDetailsComponent } from './quotes-details/quotes-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesDetailsComponent
   ],
   imports: [
     BrowserModule,
