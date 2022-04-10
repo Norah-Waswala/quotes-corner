d40d4f1ba6d471c926305e19a9a08bdb751e7a91
Norah.Waswala <norah.waswala@student.moringa.com>
2022-04-11 01:33:06 +0300

    feat:like and dislike images

--- src/app/quote-likes/quote-likes.component.ts
+++ src/app/quote-likes/quote-likes.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quotes } from '../quotes';
 @Component({
   selector: 'app-quote-likes',
   templateUrl: './quote-likes.component.html',
   styleUrls: ['./quote-likes.component.css']
 })
 export class QuoteLikesComponent implements OnInit {
-
+  numberOfLikes: number=0;
+  numberOfDislike:number=0;
+  likesImgClick(){
+    this.numberOfLikes++
+  }
+  
+  dislikeImgClick(){
+    this.numberOfDislike++
+  }
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+ 
